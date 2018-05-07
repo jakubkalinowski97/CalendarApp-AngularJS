0503ef72480c5cc0d56b0fa666cca913e5955149
Qbacki890 <jakubkalinowski97@gmail.com>
2018-05-07 10:29:19 +0200

    Daily calendar

--- components/calendarToDo.js
+++ components/calendarToDo.js
@@ -11,12 +11,15 @@ function calendarCtrl($scope, $element, $attrs) {
     ctrl.iterator = 0;
     // ctrl.task = {name: "", people: "", begDate: "", begTime: "", endDate: "", endTime: ""};
     ctrl.people;
-    $scope.options = ['Birthday', 'Meeting', 'Trip','Date' ,'Other'];
+    ctrl.options = ['Birthday', 'Meeting', 'Trip','Date' ,'Other'];
+    ctrl.today;
 
     var start = ctrl.selected.clone();
     start.date(1);
     resetTime(start.date(1));
     buildMonth(start, ctrl.month);
+
+
 
     ctrl.next = function () {
         var next = ctrl.month.clone();
@@ -38,12 +41,16 @@ function calendarCtrl($scope, $element, $attrs) {
         ctrl.task.begDate = day.date.toDate();
         ctrl.task.endDate = day.date.toDate();
         // ctrl.task.begTime = moment().toDate();
-        console.log(ctrl.dayTask.date);
-        console.log(day);
+        //console.log(ctrl.dayTask.date);
+        //console.log(day);
     }
 
     ctrl.getDay2 = function(day){
         ctrl.dayTask = day;
+    }
+
+    ctrl.todayIs = function(day){
+        ctrl.today = day;
     }
 
     ctrl.deleteTask = function(task, day){
@@ -62,8 +69,8 @@ function calendarCtrl($scope, $element, $attrs) {
                 break;
             }
         }
-         console.log("ctrl.dayTask.tasks: "+ctrl.dayTask.tasks.length);
-         console.log("ctrl.storedTasks: "+ctrl.storedTasks.length);
+         //console.log("ctrl.dayTask.tasks: "+ctrl.dayTask.tasks.length);
+         //console.log("ctrl.storedTasks: "+ctrl.storedTasks.length);
     };
 
     ctrl.addTask = function(task){
@@ -71,28 +78,23 @@ function calendarCtrl($scope, $element, $attrs) {
             id: ctrl.iterator,
             name: task.name,
             people: task.people,
-            date: ctrl.dayTask.date
+            date: ctrl.dayTask.date,
+            begTime: ctrl.dayTask.date.hour(task.begTime.getHours()).minute(task.begTime.getMinutes()),
+            endTime: ctrl.dayTask.date.hour(task.endTime.getHours()).minute(task.endTime.getMinutes()),
         });
-        ctrl.dayTask.date
         ctrl.storedTasks.push({
             id: ctrl.iterator,
             name: task.name,
             people: task.people,
-            date: ctrl.dayTask.date
+            date: ctrl.dayTask.date,
+            begTime: ctrl.dayTask.date.hour(task.begTime.getHours()).minute(task.begTime.getMinutes()),
+            endTime: ctrl.dayTask.date.hour(task.endTime.getHours()).minute(task.endTime.getMinutes()),
         });
         ctrl.task = {name: "", people: "", begDate: "", begTime: "", endDate: "", endTime: ""};
         ctrl.people = "";
         ctrl.iterator++;
-        console.log(ctrl.storedTasks);
     };
 
-    ctrl.roll = "noRollUp";
-    ctrl.rollUpTasks = function(){
-        if(ctrl.roll === "noRollUp")
-            ctrl.roll = "rollUp";
-        else
-            ctrl.roll = "noRollUp";
-    }
 
     function resetTime(date) {
         return date.day(0).hour(0).minute(0).second(0).millisecond(0);
@@ -116,18 +118,42 @@ function calendarCtrl($scope, $element, $attrs) {
                 name: date.format("dd"),
                 number: date.date(),
                 date: date,
-                tasks: []
+                tasks: [],
+                hours: buildDay(date)
             });
             for(var j=0;j<ctrl.storedTasks.length;j++){
                 console.log(ctrl.storedTasks[j].date,days[i].date);
-                if(ctrl.storedTasks[j].date.format("dddd, MMMM Do YYYY, h:mm:ss a")==days[i].date.format("dddd, MMMM Do YYYY, h:mm:ss a")){
+                if(ctrl.storedTasks[j].date.format("dddd, MMMM Do YYYY")==days[i].date.format("dddd, MMMM Do YYYY")){
                     days[i].tasks.push(ctrl.storedTasks[j]);
+                    //console.log(days[i].date);
+                    for(let k = 0;k < 24;k++){
+                        if(ctrl.storedTasks[j].begTime==days[i].hours[k].date.format("kk")){
+                            days[i].hours[k].tasks.push(ctrl.storedTasks[j]);
+                            console.log("Przypisano do godziny.");
+                        }
+                    }
+                    console.log("Przypisano do dnia.");
                 }
             }
             date = date.clone();
             date.add(1, "d");
+            //console.log(days[i].hours[1].name);
         }
         return days;
+    }
+
+    function buildDay(date){
+        var hours = [];
+        for(let i = 0; i < 24; i++){
+            hours.push({
+                name: date.format("D M YYYY kk:mm"),
+                date: date,
+                tasks: []
+            });
+            date = date.clone();
+            date.add(1, "h");
+        }
+        return hours;
     }
 }
 
