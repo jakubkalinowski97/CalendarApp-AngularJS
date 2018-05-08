74720c1b4607c952de5dda01a0825f616ebe15e6
Jakub Kalinowski <jakubkalinowski97@gmail.com>
2018-05-08 18:39:43 +0200

    Podział na komponenty i kontrolery, dodany serwis i routing.

--- components/calendarToDo.js
+++ components/calendarToDo.js
@@ -1,140 +1,124 @@
-var app = angular.module("CalendarToDo", []);
+var app = angular.module("CalendarToDo", ['ngRoute']);
 
-function calendarCtrl($scope, $element, $attrs) {
-    var ctrl = this;
-    ctrl.day = moment();
-    ctrl.selected = resetTime(moment());
-    ctrl.month = ctrl.selected.clone();
-    ctrl.dayTask;
-    ctrl.storedTasks=[];
-    ctrl.task = {name: "", people: "", begTime: "", endTime: ""};
-    ctrl.iterator = 0;
-    ctrl.people;
-    ctrl.options = [
+app.config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.
+        when('/help', {
+            templateUrl: 'views/help.html',
+        }).
+        when('/authors', {
+            templateUrl: 'views/authors.html',
+        }).
+        otherwise({
+            templateUrl: 'views/logo.html',
+        })
+    }]);
+
+app.factory('myService' ,function () {
+
+        var iterator = 0;
+        var storedTasks = [];
+        var options = [
         {name: 'Birthday', icon: "fa fa-birthday-cake"},
         {name: 'Meeting', icon: "fa fa-users"},
         {name: 'Trip', icon: "fa fa-plane"},
         {name: 'Other', icon: "fa fa-calendar-alt"}];
-    ctrl.today= {
-        name: moment().format("dd"),
-        number: moment().date(),
-        date: moment(),
-        tasks: [],
-        hours: buildDay(resetTime(moment()))
-    };
-    ctrl.search;
+        return{
+            getIterator: function(){
+                return iterator;
+            },
+            getOptions: function() {
+                return options
+            },
+            increaseIterator: function(){
+                iterator++;
+            },
+            resetTime: function (date) {
+                return date.day(0).hour(0).minute(0).second(0).millisecond(0);
+            },
+            buildDay: function (date) {
+                var hours = [];
+                for(let i = 0; i < 24; i++){
+                    hours.push({
+                        name: date.format("kk:mm"),
+                        date: date,
+                    });
+                    date = date.clone();
+                    date.add(1, "h");
+                }
+                return hours;
+            },
+            getStoredTasks: function () {
+                return storedTasks;
+            },
+            setStoredTasks: function (task,dayTask) {
+              storedTasks.push({
+                  id: iterator,
+                  name: task.name,
+                  people: task.people,
+                  date: dayTask.date,
+                  category: task.category,
+                  begTime: dayTask.date.clone().hour(task.begTime.getHours()-1).minute(task.begTime.getMinutes()),
+                  endTime: dayTask.date.clone().hour(task.endTime.getHours()-1).minute(task.endTime.getMinutes()),
+              })
+            },
+        }
+})
+
+
+app.component("calendarMonthly",{
+    templateUrl: 'views/calendarMonthly.html',
+    controller: 'monthlyCtrl',
+    bindings: {
+        "today" : '=',
+        "search" : '='
+    }
+});
+
+app.component("calendar", {
+    templateUrl: 'views/calendar.html',
+    controller: 'calendarCtrl'
+});
+
+app.component("calendarDaily",{
+    templateUrl: 'views/calendarDaily.html',
+    controller: 'dailyCtrl',
+    bindings:{
+        "today": '='
+    }
+});
+
+
+app.controller("monthlyCtrl",['$scope', 'myService', function ($scope, myService) {
+
+    var ctrl = this;
+    ctrl.day = moment();
+    ctrl.selected = myService.resetTime(moment());
+    ctrl.month = ctrl.selected.clone();
+    ctrl.task = {name: "", people: "", begTime: "", endTime: ""};
+    ctrl.options = myService.getOptions();
+    ctrl.storedTasks=myService.getStoredTasks();
+
 
     var start = ctrl.selected.clone();
     start.date(1);
-    resetTime(start.date(1));
+    myService.resetTime(start.date(1));
     buildMonth(start, ctrl.month);
-
 
 
     ctrl.next = function () {
         var next = ctrl.month.clone();
-        resetTime(next.month(next.month() + 1).date(1));
+        myService.resetTime(next.month(next.month() + 1).date(1));
         ctrl.month.month(ctrl.month.month() + 1);
         buildMonth(next, ctrl.month);
     };
 
     ctrl.previous = function () {
         var previous = ctrl.month.clone();
-        resetTime(previous.month(previous.month() - 1).date(1));
+        myService.resetTime(previous.month(previous.month() - 1).date(1));
         ctrl.month.month(ctrl.month.month() - 1);
         buildMonth(previous, ctrl.month);
     };
-
-    ctrl.getDay = function(day){
-        ctrl.dayTask = day;
-        ctrl.task.begDate = day.date.toDate();
-        ctrl.task.endDate = day.date.toDate();
-    }
-
-    ctrl.getDay2 = function(day){
-        ctrl.dayTask = day;
-    }
-
-    ctrl.todayIs = function(day){
-        ctrl.today = day;
-    }
-
-    ctrl.deleteTask = function(task, day){
-        ctrl.dayTask = day;
-        console.log(ctrl.dayTask);
-
-        for(var i = 0; i < ctrl.dayTask.tasks.length; i++) {
-            if(ctrl.dayTask.tasks[i].id == task.id) {
-                ctrl.dayTask.tasks.splice(i, 1);
-                break;
-            }
-        }
-        for(var i = 0; i < ctrl.storedTasks.length; i++) {
-            if(ctrl.storedTasks[i].id == task.id) {
-                ctrl.storedTasks.splice(i, 1);
-                break;
-            }
-        }
-         //console.log("ctrl.dayTask.tasks: "+ctrl.dayTask.tasks.length);
-         //console.log("ctrl.storedTasks: "+ctrl.storedTasks.length);
-    };
-
-    ctrl.addTask = function(task){
-        ctrl.dayTask.tasks.push({
-            id: ctrl.iterator,
-            name: task.name,
-            people: task.people,
-            date: ctrl.dayTask.date,
-            category: task.category,
-            begTime: ctrl.dayTask.date.clone().hour(task.begTime.getHours()).minute(task.begTime.getMinutes()),
-            endTime: ctrl.dayTask.date.clone().hour(task.endTime.getHours()).minute(task.endTime.getMinutes()),
-        });
-        // console.log(ctrl.dayTask.date.clone().hour(task.begTime.getHours()).minute(task.begTime.getMinutes()));
-        ctrl.storedTasks.push({
-            id: ctrl.iterator,
-            name: task.name,
-            people: task.people,
-            date: ctrl.dayTask.date,
-            category: task.category,
-            begTime: ctrl.dayTask.date.clone().hour(task.begTime.getHours()-1).minute(task.begTime.getMinutes()),
-            endTime: ctrl.dayTask.date.clone().hour(task.endTime.getHours()-1).minute(task.endTime.getMinutes()),
-        });
-        console.log(task);
-        ctrl.clearTask();
-        ctrl.iterator++;
-
-    };
-
-
-    ctrl.clearTask = function(){
-        ctrl.task = {name: "", people: "", date: "", begTime: "", endTime: "", category:""};
-    }
-
-
-    ctrl.getTask = function(task){
-        ctrl.task = task;
-    };
-
-    ctrl.editTask = function(task){
-        for(var i = 0; i < ctrl.dayTask.tasks.length; i++) {
-            if(ctrl.dayTask.tasks[i].id == task.id) {
-                //console.log("id "+ctrl.dayTask.tasks[i].id+" id2: "+task.id);
-                ctrl.dayTask.tasks[i] = task;
-                break;
-            }
-        }
-        for(var i = 0; i < ctrl.storedTasks.length; i++) {
-            if(ctrl.storedTasks[i].id == task.id) {
-                ctrl.storedTasks[i] = task;
-                break;
-            }
-        }
-        ctrl.clearTask();
-    };
-
-    function resetTime(date) {
-        return date.day(0).hour(0).minute(0).second(0).millisecond(0);
-    }
 
     function buildMonth(start, month) {
         ctrl.weeks = [];
@@ -155,12 +139,12 @@ function calendarCtrl($scope, $element, $attrs) {
                 number: date.date(),
                 date: date,
                 tasks: [],
-                hours: buildDay(date)
+                hours: myService.buildDay(date)
             });
-            for(var j=0;j<ctrl.storedTasks.length;j++){
+            for(var j=0;j<myService.getStoredTasks().length;j++){
                 //console.log(ctrl.storedTasks[j].date,days[i].date);
-                if(ctrl.storedTasks[j].date.format("dddd, MMMM Do YYYY")==days[i].date.format("dddd, MMMM Do YYYY")){
-                    days[i].tasks.push(ctrl.storedTasks[j]);
+                if(myService.getStoredTasks()[j].date.format("dddd, MMMM Do YYYY")==days[i].date.format("dddd, MMMM Do YYYY")){
+                    days[i].tasks.push(myService.getStoredTasks()[j]);
                 }
             }
             date = date.clone();
@@ -170,21 +154,100 @@ function calendarCtrl($scope, $element, $attrs) {
         return days;
     }
 
-    function buildDay(date){
-        var hours = [];
-        for(let i = 0; i < 24; i++){
-            hours.push({
-                name: date.format("kk:mm"),
-                date: date,
-            });
-            date = date.clone();
-            date.add(1, "h");
-        }
-        return hours;
+    ctrl.getDay = function(day){
+        ctrl.dayTask = day;
+        ctrl.task.begDate = day.date.toDate();
+        ctrl.task.endDate = day.date.toDate();
     }
-}
 
-app.component("calendarMonthly", {
-    templateUrl: 'views/calendarMonthly.html',
-    controller: calendarCtrl
+    ctrl.getDay2 = function(day){
+        ctrl.dayTask = day;
+    }
+
+    ctrl.deleteTask = function(task, day){
+        ctrl.dayTask = day;
+        console.log(ctrl.dayTask);
+
+        for(var i = 0; i < ctrl.dayTask.tasks.length; i++) {
+            if(ctrl.dayTask.tasks[i].id == task.id) {
+                ctrl.dayTask.tasks.splice(i, 1);
+                break;
+            }
+        }
+        for(var i = 0; i < myService.getStoredTasks().length; i++) {
+            if(myService.getStoredTasks()[i].id == task.id) {
+                myService.getStoredTasks().splice(i, 1);
+                break;
+            }
+        }
+        //console.log("ctrl.dayTask.tasks: "+ctrl.dayTask.tasks.length);
+        //console.log("ctrl.storedTasks: "+ctrl.storedTasks.length);
+    };
+
+    ctrl.addTask = function(task){
+        ctrl.dayTask.tasks.push({
+            id: myService.getIterator(),
+            name: task.name,
+            people: task.people,
+            date: ctrl.dayTask.date,
+            category: task.category,
+            begTime: ctrl.dayTask.date.clone().hour(task.begTime.getHours()).minute(task.begTime.getMinutes()),
+            endTime: ctrl.dayTask.date.clone().hour(task.endTime.getHours()).minute(task.endTime.getMinutes()),
+        });
+        // console.log(ctrl.dayTask.date.clone().hour(task.begTime.getHours()).minute(task.begTime.getMinutes()));
+        myService.setStoredTasks(task,ctrl.dayTask);
+        console.log(task);
+        ctrl.clearTask();
+        myService.increaseIterator();
+    };
+
+    ctrl.clearTask = function(){
+        ctrl.task = {name: "", people: "", date: "", begTime: "", endTime: "", category:""};
+    }
+
+    ctrl.getTask = function(task){
+        ctrl.task = task;
+    };
+
+    ctrl.editTask = function(task){
+        for(var i = 0; i < ctrl.dayTask.tasks.length; i++) {
+            if(ctrl.dayTask.tasks[i].id == task.id) {
+                //console.log("id "+ctrl.dayTask.tasks[i].id+" id2: "+task.id);
+                ctrl.dayTask.tasks[i] = task;
+                break;
+            }
+        }
+        for(var i = 0; i < ctrl.storedTasks.length; i++) {
+            if(myService.getStoredTasks().id == task.id) {
+                myService.setStoredTasks(task);
+                break;
+            }
+        }
+        ctrl.clearTask();
+    };
+
+
+}]);
+
+app.controller("calendarCtrl", function($scope, myService) {
+    var ctrl = this;
+    ctrl.options = myService.getOptions();
+    ctrl.dayTask;
+    ctrl.storedTasks=myService.getStoredTasks();
+    ctrl.people;
+    ctrl.search = {};
+    ctrl.today = {
+        name: moment().format("dd"),
+        number: moment().date(),
+        date: moment(),
+        tasks: [],
+        hours: myService.buildDay(myService.resetTime(moment()))
+    };
 });
+
+app.controller("dailyCtrl",['$scope', 'myService', function ($scope, myService) {
+    var ctrl = this;
+}]);
+
+
+
